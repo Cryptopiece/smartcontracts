5137ff680883e05335909f0b3e27969c205bf56a
Ha Bogay <habogay@gmail.com>
2021-05-04 21:19:37 +0700

    add function for test smart contract

--- contracts/token/FTXFEshare.sol
+++ contracts/token/FTXFEshare.sol
@@ -11,6 +11,7 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
 
 contract FTXFEshare is ERC20("FTXFEshare", "ESHARE"), ERC20Burnable , Ownable {
     using SafeMath for uint256;
+    address[] holders;
 
     constructor(address owner) {
         transferOwnership(owner);
@@ -18,10 +19,31 @@ contract FTXFEshare is ERC20("FTXFEshare", "ESHARE"), ERC20Burnable , Ownable {
 
     function mint(address to, uint256 amount) public onlyOwner {
        _mint(to,amount);
+        uint exists = 0;
+        for (uint i=0; i<holders.length; i++) 
+        {
+            if(holders[i]==to)exists = 1; 
+        }
+        if(exists == 0){
+            holders.push(to);
+        }
+       
+        
     }
+
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal override(ERC20) {
         super._beforeTokenTransfer(from, to, amount);
     }
+    
+    function getHoldersSize() public view  returns(uint count){
+        return holders.length;    
+    }
 
-   
+    function getHolders() public view returns(address[] memory holders)
+    {
+        return holders;
+    }
+
+    
+
 }
--- test/eshare_test.ts
+++ test/eshare_test.ts
@@ -51,5 +51,44 @@ describe('eshare contract', function() {
         
         chai.expect(BigNumber.from(10).pow(18).mul(1000).eq(balanceOfOwner)).true;
     });
- 
+
+    it ('Balance of 0x606f86D79edE6C04DD583FE89b64eF010405D462 to be increase to 2000 when mint', async function() {
+        const [deployer, owner] = await ethers.getSigners();
+        
+        const Token = await ethers.getContractFactory("FTXFEshare", deployer);
+        const token = await Token.deploy(deployer.address);
+        
+        await token.mint("0x606f86D79edE6C04DD583FE89b64eF010405D462", (BigNumber.from(10).pow(18).mul(1000)));
+        await token.mint("0x606f86D79edE6C04DD583FE89b64eF010405D462", (BigNumber.from(10).pow(18).mul(1000)));
+        const balanceOfTheAddress = await token.balanceOf("0x606f86D79edE6C04DD583FE89b64eF010405D462");
+        
+        chai.expect(BigNumber.from(10).pow(18).mul(2000).eq(balanceOfTheAddress)).true;
+    });
+
+     it ('Get size of Holders', async function() {
+        const [deployer, owner] = await ethers.getSigners();
+        
+        const Token = await ethers.getContractFactory("FTXFEshare", deployer);
+        const token = await Token.deploy(deployer.address);
+        
+        await token.mint("0x606f86D79edE6C04DD583FE89b64eF010405D462", (BigNumber.from(10).pow(18).mul(1000)));
+        await token.mint("0x807077b53eFa797D64E2158704427ac82a055aFC", (BigNumber.from(10).pow(18).mul(5000)));
+        const sizeOfHolders = await token.getHoldersSize();
+        chai.expect(BigNumber.from(2).eq(sizeOfHolders)).true;
+    }); 
+
+    it ('Get size of Holders that pushing exists address', async function() {
+        const [deployer, owner] = await ethers.getSigners();
+        
+        const Token = await ethers.getContractFactory("FTXFEshare", deployer);
+        const token = await Token.deploy(deployer.address);
+        
+        await token.mint("0x606f86D79edE6C04DD583FE89b64eF010405D462", (BigNumber.from(10).pow(18).mul(1000)));
+        await token.mint("0x606f86D79edE6C04DD583FE89b64eF010405D462", (BigNumber.from(10).pow(18).mul(1000)));
+        const sizeOfHolders = await token.getHoldersSize();
+        chai.expect(BigNumber.from(1).eq(sizeOfHolders)).true;
+    }); 
+
+
+
 });
