26370d9cb33664f898c354fc525f813bba867a2b
NgocBD <buidinhngoc.aiti@gmail.com>
2021-07-21 10:05:08 +0700

    some new test case and logic for mint limit

--- contracts/token/CoinTop360.sol
+++ contracts/token/CoinTop360.sol
@@ -12,21 +12,24 @@ import "openzeppelin-solidity/contracts/token/ERC20/ERC20Burnable.sol";
 contract CoinTop360 is ERC20("CoinTop360", "CT360"), ERC20Burnable ,
         ERC20Capped( 360000000 * (10**uint256(18))), Ownable {
     using SafeMath for uint256;
-    uint[] timeLock  = [1626777020, 1626777220, 1626785020, 1726775020]; 
+    uint256 startTime = 1627146000; // 25 Jul 2021
+    uint256 period = 360 days; 
     constructor() {
 	    _mint(msg.sender, 1000000 * (10**uint256(18)));
         transferOwnership(msg.sender);
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
-      
+       require(maxTotalSupply(block.timestamp)>totalSupply()+amount,"Can't mint more than maxTotalSupply");
        _mint(to,amount);
     }
-    function findIndex(uint time)  public view returns (uint index){
-         for (uint i = 0 ; i < timeLock.length; i++) {
-         if(timeLock[i]>time) return i; 
-      }
-      
+    function countDuration(uint256 time)  public view returns (uint256 count){
+        count = (time - startTime)/period;
+        return count;      
+    }
+    function maxTotalSupply(uint256 time)  public view returns (uint256 maxSupply){
+        maxSupply = (countDuration(time) + 2)*6000000*(10**uint256(18));
+        return maxSupply;      
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 amount) internal override(ERC20, ERC20Capped) {
--- test/test_token.ts
+++ test/test_token.ts
@@ -38,25 +38,66 @@ describe('token contract', function() {
         chai.expect(BigNumber.from(10).pow(18).mul(1000000).eq(pre_mint)).true;
     });
 
-    it('index must be 1 with 1626777020', async function() {
+    it('Duration must be 0 with 1627146000', async function() {
         const [owner] = await ethers.getSigners();
 
         const Token = await ethers.getContractFactory("CoinTop360", owner);
         const token = await Token.deploy();
-        const index = await token.findIndex(1626777020);
-        console.log(index);
-        chai.expect(index==1).true;
+        const Duration = await token.countDuration(1627146000);
+       
+        chai.expect(Duration==0).true;
     });
 
-    it('index must be 2 with 1626777222', async function() {
+    it('Duration must be 1 with 1627146000 plus 1 years', async function() {
+
+        var startDate = "25 Jul 2021";
+        var numOfYears = 1;
+        var testDate = new Date(startDate);
+        testDate.setFullYear(testDate.getFullYear() + numOfYears);
+        testDate.setDate(testDate.getDate() + 20);
+
+
         const [owner] = await ethers.getSigners();
 
         const Token = await ethers.getContractFactory("CoinTop360", owner);
         const token = await Token.deploy();
-        const index = await token.findIndex(1626777222);
-        console.log(index);
-        chai.expect(index==2).true;
+        const Duration = await token.countDuration(testDate.getTime()/1000);
+       
+        chai.expect(Duration==1).true;
     });
-    
- 
-});
+    it('mint 3000000 must be ok', async function() {
+
+      
+        const [owner] = await ethers.getSigners();
+
+        const Token = await ethers.getContractFactory("CoinTop360", owner);
+        const token = await Token.deploy();
+        await token.mint(owner.address,BigNumber.from(10).pow(18).mul(3000000));
+       
+        const totalSupply = await token.totalSupply();
+        
+        chai.expect(BigNumber.from(10).pow(18).mul(4000000).eq(totalSupply)).true;
+    });
+
+    it('mint 14000000 must be fail', async function() {
+
+        var testDate = new Date("26 Jul 2021");
+        
+        const [owner] = await ethers.getSigners();
+
+        const Token = await ethers.getContractFactory("CoinTop360", owner);
+        const token = await Token.deploy();
+        await ethers.provider.send("evm_setNextBlockTimestamp", [testDate.getTime()/1000])
+        await ethers.provider.send("evm_mine",[]) 
+        const Duration = await token.countDuration(testDate.getTime()/1000);
+        console.log(Duration);
+        const maxTotalSupply = await token.maxTotalSupply(testDate.getTime()/1000);
+        console.log(maxTotalSupply);
+
+        try {
+            await token.mint(owner.address,BigNumber.from(10).pow(18).mul(14000000));
+          } catch (err) {
+            chai.expect(true).true;
+          }
+      });
+ });
